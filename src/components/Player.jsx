0565c9b2424751b2cf6ca471241cce9d9c55b1e5
krystimer <krystimer@gmail.com>
2024-01-06 17:10:31 +0100

    Initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,23 @@
+import { useRef } from "react";
+import { useState } from "react";
+
+export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayer,setEneteredPlayer] = useState(null);
+
+
+   function handleClick(){
+    setEneteredPlayer(playerName.current.value);
+      }
+
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayer ? enteredPlayer : 'unknown enity'}</h2>
+      <p>
+        <input ref ={playerName} type="text" />
+        <button  onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
